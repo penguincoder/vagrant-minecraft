96cf95c1473ffa9c88f435801b3014444370c3ad
Andrew Coleman <penguincoder@gmail.com>
2017-06-12 16:35:18 -0400

    use new make system

--- provision.sh
+++ provision.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-MC_HOME=/home/alpine
+MC_HOME=${MC_HOME:-/home/alpine}
 
-docker run \
-  --restart always \
-  --detach \
-  --name minecraft \
-  --publish 25565:25565 \
-  --env EULA=true \
-  --env DIFFICULTY=normal \
-  --env ALLOW_NETHER=true \
-  --env ANNOUNCE_PLAYER_ACHIEVEMENTS=true \
-  --env GENERATE_STRUCTURES=true \
-  --env MEMORY=2G \
-  --volume ${MC_HOME}/minecraft:/data \
-  itzg/minecraft-server
-
-cd $(dirname $0)/overviewer && \
-  docker build -t overviewer . && \
-  docker run \
-    --detach \
-    --restart always \
-    --name overviewer \
-    --volume ${MC_HOME}/minecraft:/data \
-    --volume ${MC_HOME}/overviewer:/map \
-    overviewer
-
+docker pull itzg/minecraft-server
 docker pull zzrot/alpine-caddy
-docker run \
-  --detach \
-  --restart always \
-  --name caddy \
-  --publish 8088:80 \
-  --volume ${MC_HOME}/overviewer:/var/www/html \
-  zzrot/alpine-caddy
+make -C $(dirname $0)/minecraft-server
+make -C $(dirname $0)/overviewer
+make -C $(dirname $0)/caddy
