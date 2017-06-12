5cd7dc361211e084eb246e2412441b177d2a9279
Andrew Coleman <penguincoder@gmail.com>
2017-06-12 16:34:33 -0400

    add additional renders for the end, nether, and caves

--- overviewer/config.py
+++ overviewer/config.py
@@ -30,3 +30,45 @@ renders["West"] = {
 	'rendermode': rendermode,
 	'northdirection': 'lower-left'
 }
+renders["Nether-North"] = {
+	'world': world_name,
+	'title': 'Nether-North',
+	'dimension': 'nether',
+	'rendermode': 'nether_smooth_lighting',
+	'northdirection': 'upper-left'
+}
+renders["Nether-South"] = {
+	'world': world_name,
+	'title': 'Nether-South',
+	'dimension': 'nether',
+	'rendermode': 'nether_smooth_lighting',
+	'northdirection': 'lower-right'
+}
+renders["End-North"] = {
+	'world': world_name,
+	'title': 'End-North',
+	'dimension': 'end',
+	'rendermode': 'smooth_lighting',
+	'northdirection': 'upper-left'
+}
+renders["End-South"] = {
+	'world': world_name,
+	'title': 'End-South',
+	'dimension': 'end',
+	'rendermode': 'smooth_lighting',
+	'northdirection': 'lower-right'
+}
+renders["Caves-North"] = {
+	'world': world_name,
+	'title': 'Caves-North',
+	'dimension': "overworld",
+	'rendermode': 'cave',
+	'northdirection': 'upper-left'
+}
+renders["Caves-South"] = {
+	'world': world_name,
+	'title': 'Caves-South',
+	'dimension': "overworld",
+	'rendermode': 'cave',
+	'northdirection': 'lower-right'
+}
